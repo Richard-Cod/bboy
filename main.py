2fc405b6e8fbeeb1c754100545e6ddc343ef532e
Richard Bathiebo <richardbathiebo@MacBook-Pro-de-Richard.local>
2023-09-04 19:56:47 +0200

    commit

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import os
 # De ton coté tu n'as pas besoin de ça vu que la requete passe chez toi 
 file_name = 'file.html'
 file_path = os.path.join(os.path.dirname(__file__), file_name)
-
 with open(file_path, 'r', encoding='utf-8') as file:
     html_content = file.read()
     price_range = Selector(text=html_content).xpath('//div[@class="SrqKb"]/text()').get()
