aaa394cf4ca2190471e8e3df62b917c4a1092173
Richard Bathiebo <richardbathiebo@MacBook-Pro-de-Richard.local>
2023-09-04 19:53:54 +0200

    commit

---
+++ main.py
@@ -0,0 +1,14 @@
+from scrapy.selector import Selector
+import os
+
+# J'ai mis le contenu html dans un fichier nommé file.html 
+# De ton coté tu n'as pas besoin de ça vu que la requete passe chez toi 🙃
+file_name = 'file.html'
+file_path = os.path.join(os.path.dirname(__file__), file_name)
+
+with open(file_path, 'r', encoding='utf-8') as file:
+    html_content = file.read()
+    price_range = Selector(text=html_content).xpath('//div[@class="SrqKb"]/text()').get()
+
+    print("result")
+    print(price_range)
