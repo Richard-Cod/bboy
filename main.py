a55497bba1b78fc3732b09d7efbcd973e6585a8b
Richard Bathiebo <richardbathiebo@MacBook-Pro-de-Richard.local>
2023-09-04 19:54:34 +0200

    commit

--- main.py
+++ main.py
@@ -9,6 +9,5 @@ file_path = os.path.join(os.path.dirname(__file__), file_name)
 with open(file_path, 'r', encoding='utf-8') as file:
     html_content = file.read()
     price_range = Selector(text=html_content).xpath('//div[@class="SrqKb"]/text()').get()
-
     print("result")
     print(price_range)
